10ef25245f09ab787fe1a05968a32fc31d641fea
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-06-11 21:12:39 +0200

    Add Sentry logging

--- app.ts
+++ app.ts
@@ -2,11 +2,14 @@ import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
 import { App } from 'homey'
+const { Log } = require('homey-log')
 
 class UponorApp extends App {
-    async onInit() {
-        this.log('UponorApp is running...')
-    }
+  async onInit() {
+    // @ts-ignore TS2339
+    this.homeyLog = new Log({ homey: this.homey })
+    this.log('UponorApp is running...')
+  }
 }
 
 module.exports = UponorApp
