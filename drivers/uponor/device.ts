6955ab86549d59fde52f247c7032882f7a4abfa8
ChrisTerBeke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-12-31 12:26:47 +0100

    Refactor entire app to reduce number of API calls

--- drivers/uponor/device.ts
+++ drivers/uponor/device.ts
@@ -1,28 +1,14 @@
-import { isIPv4 } from 'net'
 import { Device, DiscoveryResultMAC } from 'homey'
 import { UponorHTTPClient } from '../../lib/UponorHTTPClient'
+import { UponorDriver } from './driver'
 
 const POLL_INTERVAL_MS = 1000 * 60 * 1
 
 class UponorThermostatDevice extends Device {
 
-    private _syncInterval?: NodeJS.Timer
-    private _client?: UponorHTTPClient
-
     async onInit(): Promise<void> {
-        this._init()
-    }
-
-    async onAdded(): Promise<void> {
-        this._init()
-    }
-
-    async onUninit(): Promise<void> {
-        this._uninit()
-    }
-
-    onDeleted(): void {
-        this.homey.clearInterval(this._syncInterval)
+        this.homey.setInterval(this._sync.bind(this), POLL_INTERVAL_MS)
+        this.homey.setTimeout(this._sync.bind(this), 2000)
     }
 
     onDiscoveryResult(discoveryResult: DiscoveryResultMAC): boolean {
@@ -30,63 +16,21 @@ class UponorThermostatDevice extends Device {
     }
 
     async onDiscoveryAvailable(discoveryResult: DiscoveryResultMAC): Promise<void> {
-        await this._updateDiscoveredAddress(discoveryResult.address)
-        await this._updateAddress(discoveryResult.address, true)
+        await this._updateAddress(discoveryResult.address)
     }
 
     async onDiscoveryAddressChanged(discoveryResult: DiscoveryResultMAC): Promise<void> {
-        await this._updateDiscoveredAddress(discoveryResult.address)
-        await this._updateAddress(discoveryResult.address, true)
+        await this._updateAddress(discoveryResult.address)
     }
 
-    async onSettings({ newSettings }: { newSettings: { [key: string]: any } }): Promise<void> {
-        const addressUpdated = await this._updateAddress(newSettings.address as string)
-        if (!addressUpdated) throw new Error(`Could not connect to Uponor controller on IP address ${newSettings.address}`)
+    private _getClient(): UponorHTTPClient {
+        const driver = this.driver as UponorDriver
+        return driver.getClient()
     }
 
-    private _getAddress(): string | undefined {
-        const settingAddress = this.getSetting('address')
-        if (settingAddress && isIPv4(settingAddress)) return settingAddress
-        const storeAddress = this.getStoreValue('address')
-        if (storeAddress && isIPv4(storeAddress)) return storeAddress
-        return undefined
-    }
-
-    private async _updateAddress(newAddress: string, persist = false): Promise<boolean> {
-        if (newAddress.length === 0) {
-            newAddress = await this.getStoreValue('address')
-        }
-
-        if (!isIPv4(newAddress)) {
-            return false
-        }
-
-        if (persist) {
-            await this.setStoreValue('address', newAddress)
-        }
-
-        if (!this._client) return false
-        const success = await this._client.updateAddress(newAddress)
-        return success
-    }
-
-    private async _updateDiscoveredAddress(newAddress: string): Promise<void> {
-        if (newAddress.length === 0) return
-        await this.setSettings({ 'discovered_address': newAddress })
-    }
-
-    async _init(): Promise<void> {
-        const address = this._getAddress()
-        if (!address) return this.setUnavailable('No IP address configured')
-        this._client = new UponorHTTPClient(address)
-        this._syncInterval = this.homey.setInterval(this._sync.bind(this), POLL_INTERVAL_MS)
-        this.homey.setTimeout(this._sync.bind(this), 2000)
-    }
-
-    async _uninit(): Promise<void> {
-        this.homey.clearInterval(this._syncInterval)
-        this._syncInterval = undefined
-        this._client = undefined
+    private async _updateAddress(newAddress: string): Promise<boolean> {
+        const driver = this.driver as UponorDriver
+        return await driver.setIpAddress(newAddress)
     }
 
     private async _sync(): Promise<void> {
@@ -95,18 +39,19 @@ class UponorThermostatDevice extends Device {
     }
 
     private async _syncCapabilities(): Promise<void> {
-        this.registerCapabilityListener('target_temperature', this._setTargetTemperature.bind(this))
+        this._ensureCapabilityListener('target_temperature', this._setTargetTemperature.bind(this))
+    }
+
+    private _ensureCapabilityListener(capability: string, callback: (value: any) => Promise<void>): void {
+        if (this.listenerCount(capability) === 1) return
+        this.registerCapabilityListener(capability, callback)
     }
 
     private async _syncAttributes(): Promise<void> {
-        if (!this._client) return this.setUnavailable('No Uponor client')
-        const canConnect = await this._client.testConnection()
-        if (!canConnect) return this.setUnavailable('Could not connect to Uponor controller on local network')
-
         try {
-            await this._client.syncAttributes()
+            await this._getClient().syncAttributes()
             const { controllerID, thermostatID } = this.getData()
-            const data = this._client.getThermostat(controllerID, thermostatID)
+            const data = this._getClient().getThermostat(controllerID, thermostatID)
             if (!data) return this.setUnavailable('Could not find thermostat data')
             this.setAvailable()
             this.setCapabilityValue('measure_temperature', data.temperature)
@@ -114,21 +59,12 @@ class UponorThermostatDevice extends Device {
         } catch (error) {
             this.setUnavailable('Could not fetch data from Uponor controller')
         }
-
-        try {
-            const { debugEnabled } = this.getSettings()
-            if (!debugEnabled) return
-            const debug = await this._client.debug()
-            this.setSettings({ apiData: JSON.stringify(debug) })
-        } catch (error) { }
     }
 
     private async _setTargetTemperature(value: number): Promise<void> {
-        if (!this._client) return this.setUnavailable('No Uponor client')
         const { controllerID, thermostatID } = this.getData()
-
         try {
-            await this._client.setTargetTemperature(controllerID, thermostatID, value)
+            await this._getClient().setTargetTemperature(controllerID, thermostatID, value)
         } catch (error) {
             this.setUnavailable('Could not send data to Uponor controller')
         }
--- drivers/uponor/driver.ts
+++ drivers/uponor/driver.ts
@@ -1,44 +1,63 @@
 import { Driver } from 'homey'
 import { Thermostat, UponorHTTPClient } from '../../lib/UponorHTTPClient'
-import { PairSession } from 'homey/lib/Driver';
+import { PairSession } from 'homey/lib/Driver'
 
-class UponorDriver extends Driver {
+export class UponorDriver extends Driver {
 
-    private _customIpAddress: string | undefined
+    private _client?: UponorHTTPClient
+
+    getClient(): UponorHTTPClient {
+        const ip_address = this.getIpAddress()
+        if (!ip_address) throw new Error('IP address not discovered or set during pairing')
+        if (!this._client) this._client = new UponorHTTPClient(ip_address)
+        return this._client
+    }
+
+    getIpAddress(): string | undefined {
+        return this.homey.settings.get('ip_address')
+    }
+
+    async setIpAddress(ipAddress: string): Promise<boolean> {
+        return await this.getClient().updateAddress(ipAddress)
+    }
 
     async onPair(session: PairSession): Promise<void> {
+        session.setHandler('custom_ip_address', this._setCustomIpAddress.bind(this))
+        session.setHandler('list_devices', this._listDevices.bind(this))
+    }
 
-        const driver = this
+    private async _setCustomIpAddress(address: string): Promise<void> {
+        this.homey.settings.set('ip_address', address)
+    }
 
-        session.setHandler('custom_ip_address', async function (address: string) {
-            driver._customIpAddress = address
-        })
+    private async _listDevices(): Promise<any[]> {
+        
+        // when a custom IP address is set, only return devices for that address
+        const ip_address = this.getIpAddress()
+        if (ip_address) {
+            return await this._findDevices(ip_address, `custom_${new Date().getTime()}`)
+        }
 
-        session.setHandler('list_devices', async function () {
-            const discoveryStrategy = driver.getDiscoveryStrategy()
-            const discoveryResults = discoveryStrategy.getDiscoveryResults()
+        // otherwise discover devices on the network and use the first one found
+        const discoveryStrategy = this.getDiscoveryStrategy()
+        const discoveryResults = discoveryStrategy.getDiscoveryResults()
+        const controller = Object.values(discoveryResults).pop()
+        if (controller) {
+            this.setIpAddress(controller.address)
+            return await this._findDevices(controller.address, controller.id)
+        }
 
-            for await (let discoveryResult of Object.values(discoveryResults)) {
-                return await driver._findDevices(discoveryResult.address, discoveryResult.id)
-            }
-
-            if (driver._customIpAddress) {
-                const time = new Date().getTime()
-                return await driver._findDevices(driver._customIpAddress, `custom_${time}`)
-            }
-
-            return []
-        })
+        return []
     }
 
     private async _findDevices(ipAddress: string, systemID: string): Promise<any[]> {
-        const client = new UponorHTTPClient(ipAddress)
-
         try {
-            const connected = await client.testConnection()
+            const connected = await this.getClient().testConnection()
             if (!connected) return []
-            await client.syncAttributes()
-            const thermostats = Array.from(client.getThermostats().values())
+            await this.getClient().syncAttributes()
+            const debug = await this.getClient().debug()
+            this.homey.settings.set('debug_devices', JSON.stringify(debug))
+            const thermostats = Array.from(this.getClient().getThermostats().values())
             return thermostats.map(this._mapDevice.bind(this, ipAddress, systemID))
         } catch (error) {
             return []
